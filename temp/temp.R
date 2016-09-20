41727cf632155f6dc7c332a8fe0a9b621dde76e7
Leo Wang <leo42k@Leos-MacBook-Pro.local>
2016-09-20 01:20:00 -0400

    timeline is clear now; we care about image, speed, and angle

--- temp/temp.R
+++ temp/temp.R
@@ -3,10 +3,22 @@ library(imager)
 # install.packages("h5", repos = "http://www.omegahat.org/R", type="source")
 # install.packages("/Users/leo42k/Downloads/h5_0.9.2.tgz", repos = NULL, type="source")
 
+
+
+
+
+
+90870/18177
+18177/20/60/60
+
 data <- H5File("/Users/leo42k/Desktop/autodrive/comma-dataset/camera/2016-06-08--11-46-01.h5")
 names <- list.datasets(data, recursive = TRUE)
 names
 
+x <- data[names[1]]
+x@dim
+range(x[1:90870])
+x[20000:20080]
 x <- data[names[1]]
 x@dim
 y <- x[2000:2020,,,]
